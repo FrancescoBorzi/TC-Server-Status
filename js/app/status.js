1f51939ca461cbdcb5a67792f24a716816c2578c
ShinDarth <borzifrancesco@gmail.com>
2015-05-10 13:24:38 +0200

    Basic display

--- js/app/status.js
+++ js/app/status.js
@@ -10,8 +10,32 @@
 
       $scope.onlinePlayers = data.length;
 
-      if ($scope.onlinePlayers > 0)
+      if ($scope.onlinePlayers > 0) {
         $scope.players = data;
+
+        $scope.players.forEach(function(player) {
+
+          switch (player.race) {
+            case 2:
+            case 5:
+            case 6:
+            case 8:
+            case 9:
+            case 10:
+              player.faction = "horde";
+              break;
+
+            case 1:
+            case 3:
+            case 4:
+            case 7:
+            case 11:
+              player.faction = "alliance";
+              break;
+          }
+
+        });
+      }
 
     })
       .error(function(data, status, header, config) {
